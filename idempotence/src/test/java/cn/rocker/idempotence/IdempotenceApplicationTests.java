4a823e1c1e8148a51e1f18fbe1c74427bdf9a015
rocker <rocker_pg@163.com>
2019-09-28 10:27:39 +0800

    init

---
+++ idempotence/src/test/java/cn/rocker/idempotence/IdempotenceApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.rocker.idempotence;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IdempotenceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
